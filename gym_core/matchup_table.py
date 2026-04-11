dfee850941e9d6f98390a23ef8b215dbf6714cf6
gtheming <heming.g@northeastern.edu>
2026-04-11 17:45:23 -0400

    revamped matchup table

--- gym_core/matchup_table.py
+++ gym_core/matchup_table.py
@@ -1,5 +1,9 @@
 from __future__ import annotations
 from typing import TypeAlias
 from gym_core.player import PlayerID
+from gym_core.cards import Card
 
-MatchupDict: TypeAlias = dict[PlayerID, PlayerID]
+MatchupPair: TypeAlias = tuple[PlayerID, PlayerID]
+CardPair: TypeAlias = tuple[Card, Card]
+
+MatchupDict: TypeAlias = dict[MatchupPair, CardPair]
