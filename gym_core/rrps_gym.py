f379b94a921d5f7800be45570d2bbd18d6a44368
Russ738 <russjwelch@gmail.com>
2026-04-16 14:15:52 -0400

    start work on abstracting functionality in RestrictedRPSEnv

---
+++ gym_core/rrps_gym.py
@@ -0,0 +1,16 @@
+from abc import ABC, abstractmethod
+from gym_core.info import Info
+import gymnasium as gym
+
+
+class RRPSEnvCore(gym.Env, ABC):
+
+    @abstractmethod
+    def _get_obs(self): ...
+
+    """return the observation used in the hash function/state key"""
+
+    @abstractmethod
+    def _get_info(self) -> Info: ...
+
+    """return the info object used for reviewing/inspecting/degugging the enviroment"""
